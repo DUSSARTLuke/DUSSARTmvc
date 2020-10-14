c003e3fd949bf667cc872d28a5fd8b35853ec67d
DUSSART Luke <lukedussart@hotmail.fr>
2020-10-14 16:43:04 +0200

    ajout paramtre connexion

---
+++ src/Model/GestionClientModel.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace APP\Model; 
+/**
+ * Description of GestionClientModel
+ *
+ * @author dussart.luke
+ */
+class GestionClientModel
+{
+  //put your code here
+}
